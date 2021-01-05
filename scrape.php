b7a4aab750efa16e42ff2768d9a63066278eddd5
Dominik Homberger <dominikhlbg@gmail.com>
2021-01-05 21:48:44 +0100

    update

--- scrape.php
+++ scrape.php
@@ -1,25 +1,26 @@
 <?php
-// MIT License
+
+// Copyright (c) 2012-2021 Dominik Homberger (https://github.com/dominikhlbg/allris-scraper)
+// Copyright (c) 2012 Marian Steinbach (https://github.com/marians/cologne-ris-scraper)
 //
-// Copyright (c) 2012-2021 Dominik Homberger (dominikhlbg@gmail.com)
+// Hiermit wird unentgeltlich jeder Person, die eine Kopie der Software und 
+// der zugehörigen Dokumentationen (die "Software") erhält, die Erlaubnis 
+// erteilt, sie uneingeschränkt zu benutzen, inklusive und ohne Ausnahme, dem
+// Recht, sie zu verwenden, kopieren, ändern, fusionieren, verlegen 
+// verbreiten, unterlizenzieren und/oder zu verkaufen, und Personen, die diese 
+// Software erhalten, diese Rechte zu geben, unter den folgenden Bedingungen:
 //
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
+// Der obige Urheberrechtsvermerk und dieser Erlaubnisvermerk sind in allen 
+// Kopien oder Teilkopien der Software beizulegen.
 //
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
+// Die Software wird ohne jede ausdrückliche oder implizierte Garantie 
+// bereitgestellt, einschließlich der Garantie zur Benutzung für den
+// vorgesehenen oder einen bestimmten Zweck sowie jeglicher Rechtsverletzung, 
+// jedoch nicht darauf beschränkt. In keinem Fall sind die Autoren oder 
+// Copyrightinhaber für jeglichen Schaden oder sonstige Ansprüche haftbar zu 
+// machen, ob infolge der Erfüllung eines Vertrages, eines Delikts oder anders 
+// im Zusammenhang mit der Software oder sonstiger Verwendung der Software 
+// entstanden.
 
 error_reporting(0);
 set_time_limit(0);
